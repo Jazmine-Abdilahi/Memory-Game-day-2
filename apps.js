e99d318df312d17a190e9331b4e30ad4be26e47a
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-06-22 14:27:34 -0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+      {
+        name: 'fries',
+        img: 'fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'hotdog.png'
+      },     {
+        name: 'fries',
+        img: 'fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: 'cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: 'ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: 'pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: 'milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: 'hotdog.png'
+      },
+     
+     
+    
